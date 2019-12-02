71087dd2978e57f2a74b4c6a56d68d21fbc596c0
qusai <qusaifares@gmail.com>
2019-12-02 00:07:20 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -53,29 +53,3 @@ function createBoard() {
 };
 
 createBoard();
-
-
-//flipCard(0);
-//flipCard(1);
-
-
-
-/*
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped " + cardsInPlay[cardsInPlay.length - 1]);
-
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardsInPlay[cardsInPlay.length - 1]);
-
-if (cardsInPlay.length === 2) {
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found a match!");
-	} else {
-		alert("Sorry, try again.");
-	}
-}
-*/
-
-// if (cardsInPlay.length === 2)
